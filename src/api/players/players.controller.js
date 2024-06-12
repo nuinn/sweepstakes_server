b39962d326182261758dfb8e3000731283df5daf
nuinn <dilley.marc@gmail.com>
2024-06-12 19:50:01 +0200

    committing change to players controller

--- src/api/players/players.controller.js
+++ src/api/players/players.controller.js
@@ -1,9 +1,27 @@
 import * as playersService from './players.service.js';
 import * as teamsService from '../teams/teams.service.js';
 
+async function addTeams({ player }) {
+  const returningData = player;
+  if (player.wildcard) {
+    const wildcardData = await teamsService.populateTeams({
+      wildcard: player.wildcard,
+    });
+    returningData.wildcardData = wildcardData;
+  }
+  const teamsData = await teamsService.populateTeams({ teams: player.teams });
+  returningData.teamsData = teamsData;
+  return returningData;
+}
+
 async function getAll(req, res) {
   const players = await playersService.getAll();
-  res.json(players);
+  const playersWithTeamsPromises = players.map(async (player) => {
+    const playerWithTeams = await addTeams({ player });
+    return playerWithTeams;
+  });
+  const playersWithTeams = await Promise.all(playersWithTeamsPromises);
+  await res.json(playersWithTeams);
 }
 
 async function getByLeague(req, res) {
@@ -22,15 +40,16 @@ async function addTeam(req, res) {
   const { _id } = req.params;
   const { team, wildcard } = req.body;
   const updatedPlayer = await playersService.addTeam({ _id, team, wildcard });
-  if (updatedPlayer.wildcard) {
-    const wildcardData = await teamsService.populateTeams({
-      wildcard: updatedPlayer.wildcard,
-    });
-    updatedPlayer.wildcardData = wildcardData;
-  }
-  const teamsData = await teamsService.populateTeams({ teams: updatedPlayer.teams });
-  updatedPlayer.teamsData = teamsData;
-  res.json(updatedPlayer);
+  const updatedPlayerWithTeams = await addTeams({ player: updatedPlayer });
+  // if (updatedPlayer.wildcard) {
+  //   const wildcardData = await teamsService.populateTeams({
+  //     wildcard: updatedPlayer.wildcard,
+  //   });
+  //   updatedPlayer.wildcardData = wildcardData;
+  // }
+  // const teamsData = await teamsService.populateTeams({ teams: updatedPlayer.teams });
+  // updatedPlayer.teamsData = teamsData;
+  res.json(updatedPlayerWithTeams);
 }
 
 async function remove(req, res) {
